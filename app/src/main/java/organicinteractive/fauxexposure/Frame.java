d73dabe7507c458fee21321aa20f549cacbc88a8
AJ Gayler <austingayler@gmail.com>
2015-03-16 15:37:26 +0200

    removed unused code

--- app/src/main/java/organicinteractive/fauxexposure/Frame.java
+++ app/src/main/java/organicinteractive/fauxexposure/Frame.java
@@ -162,27 +162,6 @@ public class Frame extends Activity {
         }
     }
 
-    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-        Camera.Size result = null;
-        //shitty algorithm that makes things stretched out
-        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-            if (size.width <= width && size.height <= height) {
-                if (result == null) {
-                    result = size;
-                } else {
-                    int resultArea = result.width * result.height;
-                    int newArea = size.width * size.height;
-
-                    if (newArea > resultArea) {
-                        result = size;
-                    }
-                }
-            }
-        }
-
-        return (result);
-    }
-
     SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
         public void surfaceCreated(SurfaceHolder holder) {
         }
